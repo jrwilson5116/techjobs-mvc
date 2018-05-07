9ccb80bd8b591b91c9167cdb834d60a14b2bc705
jrwilson5116 <jrwilson5116@gmail.com>
2018-05-07 17:02:32 -0500

    adds search result handler

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import javafx.scene.control.RadioButton;
+import org.launchcode.controllers.ListController;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,5 +25,21 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "/results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute("columns", ListController.columnChoices);
+
+        ArrayList results;
+
+        if(searchType=="all")
+            results = JobData.findByValue(searchTerm);
+        else
+            results = JobData.findByColumnAndValue(searchType,searchTerm);
+
+        if(!results.isEmpty())
+            model.addAttribute("jobs", results);
+
+        return "search";
+    }
 
 }
